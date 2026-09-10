6d8e44e958230066f98a8830f42d9f82cbcd0fea
Parth Pal <parth.41570@stu.upes.ac.in>
2026-09-10 17:49:35 +0000

    Day02/q4.c

---
+++ Day02/q4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int r=5;
+int Area=3.14*r*r;
+int Circumference=2*3.14*r;
+printf("value of Area=%d", Area);
+printf("value of Circumference=%d", Circumference);
+printf("value of %d", r);
+return 0;
+}
+
