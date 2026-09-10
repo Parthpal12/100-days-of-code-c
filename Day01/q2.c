24fe8282d1de04bb12dad02a3ab7bc685e8ab071
Parth Pal <parth.41570@stu.upes.ac.in>
2026-09-10 19:13:53 +0000

    Convert Day01 folder from submodule to regular files

---
+++ Day01/q2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a=12;
+int b=17;
+int sum=a+b;
+int product=a*b;
+int diff=a-b;
+int quotient=a%b;
+printf("value of sum=%d",sum);
+printf("value of product=%d",product);
+printf("value of diff=%d",diff);
+printf("value of quotient=%d",quotient);
+printf("value of %d and %d" ,a ,b);
+return 0;
+}
+
