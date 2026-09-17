8cfe74145187210da25b77fe661b09922da2cd9e
Parth Pal <parth.41570@stu.upes.ac.in>
2026-09-17 20:22:55 +0000

    Day21/q41.c

---
+++ Day21/q41.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, first, last;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    last = n % 10;
+
+    first = n;
+    while (first >= 10) {
+        first = first / 10;
+    }
+
+    printf("First digit = %d\n", first);
+    printf("Last digit = %d\n", last);
+
+    return 0;
+}
