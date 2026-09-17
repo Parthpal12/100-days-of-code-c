c7a8d5fa4d2f69c9871c82adb90eeb760706c3b4
Parth Pal <parth.41570@stu.upes.ac.in>
2026-09-17 20:12:47 +0000

    Day20/q40.c

---
+++ Day20/q40.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    long long binary, complement = 0;
+    int digit, place = 1;
+
+    printf("Enter a binary number: ");
+    scanf("%lld", &binary);
+
+    while (binary > 0)
+    {
+        digit = binary % 10;
+
+        if (digit == 0)
+            digit = 1;
+        else
+            digit = 0;
+
+        complement = complement + digit * place;
+        place = place * 10;
+        binary = binary / 10;
+    }
+
+    printf("1's Complement = %lld", complement);
+
+    return 0;
+}
